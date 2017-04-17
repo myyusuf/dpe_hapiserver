f1bdd05af6b16bc0fb650ff9f3459a3c5d391e9c
myyusuf <myyusuf1911@gmail.com>
2017-04-17 12:05:47 +0700

    Fix read lsp

--- plugins/dpe-dashboard-api/handlers/dashboard_main.js
+++ plugins/dpe-dashboard-api/handlers/dashboard_main.js
@@ -1,81 +1,127 @@
 'use strict';
 
+const Flow = require('nimble');
+
 exports.getMainData = function(db, month, year, resultCallback) {
 
-  var query = "SELECT p.project_type, " +
+  var result = {
+  }
 
-  "SUM(rkap_ok) AS sum_rkap_ok, " +
-  "SUM(rkap_op) AS sum_rkap_op, " +
-  "SUM(rkap_lk) AS sum_rkap_lk, " +
+  var getSumProjectProgressPerMonth = function(callback){
+    var query = "SELECT p.project_type, " +
 
-  "SUM(realisasi_ok) AS sum_realisasi_ok, " +
-  "SUM(realisasi_op) AS sum_realisasi_op, " +
-  "SUM(realisasi_lk) AS sum_realisasi_lk, " +
+    "SUM(rkap_ok) AS sum_rkap_ok, " +
+    "SUM(rkap_op) AS sum_rkap_op, " +
+    "SUM(rkap_lk) AS sum_rkap_lk, " +
 
-  "SUM(prognosa_ok) AS sum_prognosa_ok, " +
-  "SUM(prognosa_op) AS sum_prognosa_op, " +
-  "SUM(prognosa_lk) AS sum_prognosa_lk " +
+    "SUM(realisasi_ok) AS sum_realisasi_ok, " +
+    "SUM(realisasi_op) AS sum_realisasi_op, " +
+    "SUM(realisasi_lk) AS sum_realisasi_lk, " +
 
-  "FROM project_progress pp " +
-  "LEFT JOIN project p ON pp.project_id = p.id " +
-  "WHERE pp.month <= ? AND pp.year = ? " +
-  "GROUP BY p.project_type ";
+    "SUM(prognosa_ok) AS sum_prognosa_ok, " +
+    "SUM(prognosa_op) AS sum_prognosa_op, " +
+    "SUM(prognosa_lk) AS sum_prognosa_lk " +
 
-  db.query(
-    query, [month, year],
-    function(err, rows) {
-      if (err) throw err;
+    "FROM project_progress pp " +
+    "LEFT JOIN project p ON pp.project_id = p.id " +
+    "WHERE pp.month <= ? AND pp.year = ? " +
+    "GROUP BY p.project_type ";
 
-      var query = "SELECT " +
+    db.query(
+      query, [month, year],
+      function(err, rows) {
+        if (err) throw callback(err);
 
-      "SUM(rkap_ok) AS sum_rkap_ok, " +
-      "SUM(rkap_op) AS sum_rkap_op, " +
-      "SUM(rkap_lk) AS sum_rkap_lk, " +
+        result.sumProjectProgressPerMonth = rows;
+        callback();
+      }
+    );
+  }
 
-      "SUM(prognosa_ok) AS sum_prognosa_ok, " +
-      "SUM(prognosa_op) AS sum_prognosa_op, " +
-      "SUM(prognosa_lk) AS sum_prognosa_lk " +
+  var getSumProjectProgressInYear = function(callback){
+    var query = "SELECT " +
 
-      "FROM project_progress pp " +
-      "LEFT JOIN project p ON pp.project_id = p.id " +
-      "WHERE pp.year = ? ";
+    "SUM(rkap_ok) AS sum_rkap_ok, " +
+    "SUM(rkap_op) AS sum_rkap_op, " +
+    "SUM(rkap_lk) AS sum_rkap_lk, " +
 
-      db.query(
-        query, [year],
-        function(err, rows2) {
-          if (err) throw err;
+    "SUM(prognosa_ok) AS sum_prognosa_ok, " +
+    "SUM(prognosa_op) AS sum_prognosa_op, " +
+    "SUM(prognosa_lk) AS sum_prognosa_lk " +
 
-          var rkapPrognosa = null;
-          if(rows2.length > 0){
-            rkapPrognosa = rows2[0];
-          }
+    "FROM project_progress pp " +
+    "LEFT JOIN project p ON pp.project_id = p.id " +
+    "WHERE pp.year = ? ";
 
-          var query = "SELECT " +
+    db.query(
+      query, [year],
+      function(err, rows) {
+        if (err) throw callback(err);
 
-          "SUM(lsp_rkap) AS sum_lsp_rkap, " +
-          "SUM(lsp_prognosa) AS sum_lsp_prognosa, " +
-          "SUM(lsp_realisasi) AS sum_lsp_realisasi " +
-
-          "FROM lsp " +
-          "WHERE year = ? ";
-
-          db.query(
-            query, [year],
-            function(err, rows3) {
-              if (err) throw err;
-
-              var lsp = null;
-              if(rows3.length > 0){
-                lsp = rows3[0];
-              }
-              resultCallback(rows, rkapPrognosa, lsp);
-            }
-          );
-
+        result.sumProjectProgressInYear = null;
+        if(rows.length > 0){
+          result.sumProjectProgressInYear = rows[0];
         }
-      );
+        callback();
+      }
+    );
+  }
 
+  var getSumLspInYear = function(callback){
+    var query = "SELECT " +
+
+    "SUM(lsp_rkap) AS sum_lsp_rkap, " +
+    "SUM(lsp_prognosa) AS sum_lsp_prognosa, " +
+    "SUM(lsp_realisasi) AS sum_lsp_realisasi " +
+
+    "FROM lsp " +
+    "WHERE year = ? ";
+
+    db.query(
+      query, [year],
+      function(err, rows) {
+        if (err) throw callback(err);
+
+        result.sumLspInYear = null
+        if(rows.length > 0){
+          result.sumLspInYear = rows[0];
+        }
+        callback();
+      }
+    );
+  }
+
+  var getLspInMonth = function(callback){
+    var query = "SELECT " +
+    "lsp_realisasi " +
+    "FROM lsp " +
+    "WHERE month = ? AND year = ? ";
+
+    db.query(
+      query, [month, year],
+      function(err, rows) {
+        if (err) throw callback(err);
+
+        result.lspInMonth = null
+        if(rows.length > 0){
+          result.lspInMonth = rows[0];
+        }
+        callback();
+      }
+    );
+  }
+
+  Flow.series([
+    getSumProjectProgressPerMonth,
+    getSumProjectProgressInYear,
+    getSumLspInYear,
+    getLspInMonth,
+    function(callback){
+      resultCallback(result);
     }
-  );
+  ], function(err){
+    console.log('Error while doing operation.');
+    throw err;
+  });
 
 };
