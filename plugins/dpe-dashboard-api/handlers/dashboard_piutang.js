c23c1c038c0647aa3c6cd3e35fe5fc970f6ee853
myyusuf <myyusuf1911@gmail.com>
2017-11-26 10:43:16 +0700

    Get piutang

---
+++ plugins/dpe-dashboard-api/handlers/dashboard_piutang.js
@@ -0,0 +1,26 @@
+exports.findPiutangData = function (request, reply) {
+  const year = request.params.year;
+  const month = request.params.month;
+  
+  this.db.query(
+    'SELECT * FROM piutang WHERE month = ? AND year = ?',
+    [month, year],
+    (err, rows) => {
+      if (err) reply(err);
+      reply(rows);
+    }
+  );
+};
+
+exports.findProyeksiData = function (request, reply) {
+  const year = request.params.year;
+  
+  this.db.query(
+    'SELECT * FROM proyeksi WHERE year = ?',
+    [year],
+    (err, rows) => {
+      if (err) reply(err);
+      reply(rows);
+    }
+  );
+};
