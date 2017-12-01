82c2f4277107f9fdd794ebf1da7bd4eeb2ee03c7
myyusuf <myyusuf1911@gmail.com>
2017-12-02 00:33:00 +0700

    Piutang

--- handlers/project_progress.js
+++ handlers/project_progress.js
@@ -1,25 +1,34 @@
 const Path = require('path');
 const Fs = require('fs');
 
-var DPEConstant = require('../config/dpe_constant.js');
+const DPEConstant = require('../config/dpe_constant.js');
 const ExcelReader = require('./excelreader');
+const PiutangExcelReader = require('./excel_reader/piutang');
+const ProyeksiExcelReader = require('./excel_reader/proyeksi');
 
 exports.upload = function(request, reply){
   // console.log(JSON.stringify(request.payload));
 
-  var fileData = request.payload.progress;
-  const targetPath = DPEConstant.FILE_UPLOAD_DIR + fileData.filename;//Path.join(__dirname, Path.basename(request.payload.upload.filename));
+  const fileData = request.payload.progress;
+  const targetPath = DPEConstant.FILE_UPLOAD_DIR + fileData.filename; // Path.join(__dirname, Path.basename(request.payload.upload.filename));
   const tempPath = fileData.path;
 
   Fs.rename(tempPath, targetPath, (err) => {
-      if (err) {
-          throw err;
-      }
+    if (err) {
+      throw err;
+    }
 
-      const callback = (result) => {
-        reply(result);
-      }
-      ExcelReader.readExcel(targetPath, this.db, callback);
-
+    const callback = (result) => {
+      reply(result);
+    };
+    // ExcelReader.readExcel(targetPath, this.db, callback);
+    PiutangExcelReader.readExcel(targetPath, this.db)
+    .then((result) => {
+      reply(result);
+    });
+    // ProyeksiExcelReader.readExcel(targetPath, this.db)
+    // .then((result) => {
+    //   reply(result);
+    // });
   });
-}
+};
