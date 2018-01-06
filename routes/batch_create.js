423caa56e81d3cc883133bc84ee463833252cb22
myyusuf <myyusuf1911@gmail.com>
2018-01-06 18:15:11 +0700

    Lsp batch create

--- routes/batch_create.js
+++ routes/batch_create.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const ProjectProgressController = require('../controllers/project_progress.js');
+const LspController = require('../controllers/lsp.js');
 
 const router = express.Router();
 
 router.post('/projectprogress', ProjectProgressController.batchCreate);
+router.post('/lsp', LspController.batchCreate);
 
 module.exports = router;
