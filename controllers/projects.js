14479a01785143b8401765fa09ca4a561a6ba29d
myyusuf <myyusuf1911@gmail.com>
2018-01-06 09:32:07 +0700

    Fix project list ordering

--- controllers/projects.js
+++ controllers/projects.js
@@ -16,6 +16,7 @@ exports.findAll = function findAll(req, res) {
         { name: { $ilike: searchText } },
       ],
     },
+    order: ['code'],
     include: [
       { model: models.ProjectType },
     ],
